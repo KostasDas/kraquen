f41d74967685ac1a8b69157cfc8b0be0788fec16
Konstantinos Daskalopoulos <kostas@Konstantinoss-MacBook-Pro.local>
2025-11-30 18:34:41 +0200

    feature: enriching api with len, clear, peek, is_empty

--- src/lib.rs
+++ src/lib.rs
@@ -30,6 +30,29 @@ impl<T> Queue<T> {
             }
         }
     }
+    
+    pub fn len(&self) -> usize {
+        self.data.len()
+    }
+    
+    pub fn is_empty(&self) -> bool {
+        self.data.is_empty()
+    }
+    
+    pub fn peek(&self) -> Option<&T> {
+        match self.mode {
+            QueueMode::FIFO => {
+                self.data.back()
+            }
+            QueueMode::LIFO => {
+                self.data.front()
+            }
+        }
+    }
+    
+    pub fn clear(&mut self) {
+        self.data.clear()
+    }
 }
 
 #[cfg(test)]
@@ -64,5 +87,34 @@ mod tests {
         assert_eq!(3, queue.pop().unwrap());
         assert_eq!(2, queue.pop().unwrap());
         assert_eq!(1, queue.pop().unwrap());
+    }
+    #[test]
+    fn test_peek_and_len() {
+        let mut lifo : Queue<usize> = Queue::new(QueueMode::LIFO);
+        lifo.push(1);
+        lifo.push(2);
+        assert_eq!(2, *lifo.peek().unwrap());
+        assert_eq!(2, lifo.len());
+
+        let mut fifo : Queue<usize> = Queue::new(QueueMode::FIFO);
+        fifo.push(1);
+        fifo.push(2);
+        assert_eq!(1, *fifo.peek().unwrap());
+        assert_eq!(2, fifo.len());
+    }
+    
+    #[test]
+    fn test_clear_and_len() {
+        let mut lifo : Queue<usize> = Queue::new(QueueMode::LIFO);
+        lifo.push(1);
+        lifo.push(2);
+        lifo.clear();
+        assert_eq!(0, lifo.len());
+
+        let mut fifo : Queue<usize> = Queue::new(QueueMode::FIFO);
+        fifo.push(1);
+        fifo.push(2);
+        fifo.clear();
+        assert_eq!(0, fifo.len());
     }
 }
